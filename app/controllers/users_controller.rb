04b1dc6f5bc6ae94e749051bdd14d6df289070ed
Christian Castillo <castillochristian3@gmail.com>
2023-03-24 09:44:44 -0400

    created sessions controller and login, logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: [:create, :index]
+    skip_before_action :authorize, only: [:create, :index]
 
-  def index 
-      render json: User.all, status: 200
-  end
+    #GET all users (read)
+    def index 
+        render json: User.all, status: :ok
+    end
 
-  #POST User (create)
-  def create
-      user = User.create!(user_params)
-      session[:user_id] = user.id
-      render json: user, status: :created
-  end
+    #POST User (create)
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
 
-  #GET logged in user (read)
-  def show
-      render json: @current_user
-  end
+    #GET logged in user (read)
+    def show
+        render json: @current_user
+    end
 
-  private
-      
-  def user_params
-      params.permit(:username, :password, :password_confirmation)
-  end
+    private
+        
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 end
