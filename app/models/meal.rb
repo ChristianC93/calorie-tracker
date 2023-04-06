127dadee6c0b15147defa4bd68acead75554dd0a
Christian Castillo <castillochristian3@gmail.com>
2023-04-06 09:29:42 -0400

    Created custom method image url in meal model, added rails defualt url in development rb, added image url to meal serializer

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,4 +6,8 @@ class Meal < ApplicationRecord
     
     validates :name, :calories, presence: true
     validates :calories, numericality: { only_integer: true }
+
+    def image_url
+        Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    end
 end
--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -2,5 +2,5 @@ class MealSerializer < ActiveModel::Serializer
   has_many :user_meals
   has_many :users
   
-  attributes :id, :name, :calories, :created_at
+  attributes :id, :name, :calories, :created_at, :image_url
 end
